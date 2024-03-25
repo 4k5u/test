3bc21af406f2b8b17ad6feb444ae2de6101cc0f2
nbtu <62731238+nbtu@users.noreply.github.com>
2024-03-25 22:58:40 +0800

    Update st.sh

--- st.sh
+++ st.sh
@@ -34,7 +34,7 @@ unreachableIds=()
                 curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} -X POST "${synctv}/api/movie/clear"
                 curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} --data-raw "${jsondata}" -X POST "${synctv}/api/movie/push"
                 echo "$userId 已推送到Sync TV, removing from list"
-                text="*提醒！！！！*\n\n${userId}已添加到SyncTV\n\n[直达地址](${synctv}/web/cinema/${roomid})\n\n"
+                text="*J哥提醒你！！！！*\n\n${userId}直播源已添加到SyncTV\n\n[直达地址，让我康康！](${synctv}/web/cinema/${roomid})\n\n"
                 text=$(echo "${text}" | sed 's/-/\\\\-/g')
                 text=$(echo "${text}" | sed 's/_/\\\\_/g')
                 curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@Sexbjlive_Chat\", \"text\":\"$text\"}" "https://api.telegram.org/${bot}/sendMessage?parse_mode=MarkdownV2"
