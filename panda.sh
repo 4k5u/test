caabeeb1ff406691eca8d6f98127e2183482296a
nbtu <62731238+nbtu@users.noreply.github.com>
2024-03-27 18:50:17 +0800

    Update panda.sh

--- panda.sh
+++ panda.sh
@@ -40,7 +40,7 @@ unreachableIds=()
                 curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} -X POST "${synctv}/api/movie/clear"
                 curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} --data-raw "${jsondata}" -X POST "${synctv}/api/movie/push"
                 echo "$userId 已推送到Sync TV, removing from list"
-                text="*J哥提醒你！！！！*\n\n${userId}直播源已添加到SyncTV\n\n[直达地址，让我康康！](${synctv}/web/cinema/${roomid})\n\n"
+                text="*J哥提醒你！！！！*\n\nPanda主播${userId}直播源已添加到SyncTV\n\n[直达地址，让我康康！](${synctv}/web/cinema/${roomid})\n\n"
                 curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@Sexbjlive_Chat\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
             elif [ "$roomid" = null ]; then
                 echo $room|jq -r .error
