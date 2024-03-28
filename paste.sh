e391889122b30bc00df87e1dfbc468e1089dfd84
nbtu <62731238+nbtu@users.noreply.github.com>
2024-03-28 20:20:47 +0800

    Update paste.sh

--- paste.sh
+++ paste.sh
@@ -6,9 +6,10 @@ content=$*
 if [[ "$content" ]];then
     share_link="https://paste.ubuntu.com"$(curl -v --data-urlencode "content=${content}" -d "poster=4k5" -d "expiration=month" -d "syntax=text" "https://paste.ubuntu.com" 2>&1 | grep "Location" | awk '{print $3}')
     # mac下
-    echo -e "\n 分享链接：\x1B[31m${share_link}\x1B[0m"
+    #echo -e "\n 分享链接：\x1B[31m${share_link}\x1B[0m"
     # linux下
-    echo -e "\n 分享链接：\e[31m${share_link}\e[0m"
+    #echo -e "\n 分享链接：\e[31m${share_link}\e[0m"
+    echo -e "分享链接：\e[31m${share_link}\e[0m"
 else
     echo "没有传入要上传的文本"
     echo -e "一般是这样：\x1B[31mbash $0 \x1B[32m\"你要上传的文本\"\x1B[0m"
