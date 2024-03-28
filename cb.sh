2c3892acdbeef224ceb040705f373f31dbdf31c1
nbtu <62731238+nbtu@users.noreply.github.com>
2024-03-28 08:43:11 +0800

    Update cb.sh

--- cb.sh
+++ cb.sh
@@ -10,7 +10,7 @@ password="$PASSWORD"
 cookie="";
 logfile="log/log_`date '+%Y%m%d'`.txt"
 #userIds=$1
-userIds="intim_mate emyii";
+userIds="intim_mate cuddles_me mazzanti_ honey_pinkgreen sexygamesx foxylovesyou kiriko_chan";
 
 userToken=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' --data-raw "{\"username\": \"${username}\",\"password\": \"${password}\"}" -X POST "${synctv}/api/user/login"|jq -r .data.token`
 echo $userToken
@@ -22,8 +22,9 @@ unreachableIds=()
         http_code=`curl -sSL -w "%{http_code}" -o /dev/null --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" https://cbjpeg.stream.highwebmedia.com/stream?room=${userId}`
         if [ "${http_code}" == 200 ]; then
             echo "开始获取直播源"
-            json=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" "https://btu-cf.gds.workers.dev/streamapi/?modelname=${userId}"` 
+            json=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" "https://cbtw.gds.workers.dev/streamapi/?modelname=${userId}"` 
             hls=`echo $json|jq -r .url`
+            img="https://cbjpeg.stream.highwebmedia.com/stream?room=${userId}&f=$(date '+%Y%m%d%H%M')"
             if [ -n "$hls"  ] &&  [ "$hls" != null ]; then
                 echo "${userId}获取成功。"
                 echo "直播源：${hls}"
@@ -35,12 +36,12 @@ unreachableIds=()
                 if [ -n "$roomid"  ] && [ "$roomid" != null ]; then
                     jsondata="{\"name\": \"${userId}\",\"url\": \"${hls}\",\"type\": \"m3u8\",\"live\": true}"
                     echo -e "$userId $roomid $roomToken $hls">> data.txt
-####################echo -e "添加$userId $hls">> $logfile
+                    echo -e "添加$userId $hls">> $logfile
                     curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} -X POST "${synctv}/api/movie/clear"
                     curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} --data-raw "${jsondata}" -X POST "${synctv}/api/movie/push"
                     echo "$userId 已推送到Sync TV, removing from list"
                     text="*J哥提醒你！！！！*\n\nChaturbate主播${userId}直播源已添加到SyncTV\n\n[直达地址，让我康康！](${synctv}/web/cinema/${roomid})\n\n"
-                    curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"1138220708\", \"caption\":\"$text\", \"photo\":\"https://cbjpeg.stream.highwebmedia.com/stream?room=${userId}\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
+                    curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@Sexbjlive_Chat\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
                 elif [ "$roomid" = null ]; then
                     echo $room|jq -r .error
                     echo "创建房间失败，可能已有同名房间，跳过此ID" 
