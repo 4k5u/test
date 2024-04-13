a25d0c1d67749e6043a0f20ba56a34d7a6b505cb
nbtu <62731238+nbtu@users.noreply.github.com>
2024-04-13 20:35:38 +0800

    Update panda.sh

--- panda.sh
+++ panda.sh
@@ -40,18 +40,28 @@ unreachableIds=()
             echo "房间id：$roomid"
             if [ -n "$roomid"  ] && [ "$roomid" != null ]; then
                 jsondata="{\"name\": \"${userId}\",\"url\": \"${hls}\",\"type\": \"m3u8\",\"live\": true}"
-                echo -e "$userId $roomid $roomToken $hls">> data.txt
-                echo -e "添加$userId $hls">> $logfile
+                #echo -e "$userId $roomid $roomToken $hls">> data.txt
+                #echo -e "添加$userId $hls">> $logfile
                 curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} -X POST "${synctv}/api/movie/clear"
                 curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} --data-raw "${jsondata}" -X POST "${synctv}/api/movie/push"
                 echo "$userId 已推送到Sync TV, removing from list"
-                text="*J哥提醒你！！！！*\n\nPanda主播${userId}直播源已添加到SyncTV\n\n[直达地址，让我康康！](${synctv}/web/cinema/${roomid})\n\n[直达地址②，再次康康！](${m3u8site}?url=${userId})\n\n"
-                curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@Sexbjlive_Chat\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
+                #text="*J哥提醒你！！！！*\n\nPanda主播${userId}直播源已添加到SyncTV\n\n[直达地址，让我康康！](${synctv}/web/cinema/${roomid})\n\n[直达地址②，再次康康！](${m3u8site}?url=${userId})\n\n"
+                #curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@Sexbjlive_Chat\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
             elif [ "$roomid" = null ]; then
                 echo $room|jq -r .error
                 echo "创建房间失败，可能已有同名房间，跳过此ID" 
             else 
                 echo "创建房间失败，跳过此ID（错误提示${room}）" 
+            fi
+            
+            if grep -q "${userId}" data.txt; then
+                echo "The UID $uid exists in data.txt"
+            else
+                echo "$userId 已推送到TG"
+                text="*J哥提醒你！！！！*\n\nPanda主播${userId}直播源已添加到SyncTV\n\n[直达地址，让我康康！](${synctv}/web/cinema/${roomid})\n\n[直达地址②，再次康康！](${m3u8site}?url=${userId})\n\n"
+                curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@Sexbjlive_Chat\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
+                echo -e "$userId $roomid $roomToken $hls">> data.txt
+                echo -e "添加$userId $hls">> $logfile
             fi
             reachableIds+=("$userId")
         else 
