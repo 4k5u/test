58758400e5ff6ec26152d46f8f350be6315c25e6
nbtu <62731238+nbtu@users.noreply.github.com>
2024-04-24 03:11:20 +0800

    Update pd.sh

--- pd.sh
+++ pd.sh
@@ -78,7 +78,7 @@ echo "${#onlineIds[@]}"
 watchIds="jjine0127 howru010 emforhs1919 hyuna0721 lovesong2 20152022 jenny9999 choyunkyung jinricp pandaex happyy2 4ocari na2ppeum onlyone521 imissy0u moem9e9 likecho cool3333 100472 lovemeimscared starsh2802 imgroot5 okzzzz eli05021212 ohhanna dmsdms1247 54soda ajswl12 qwas33 getme1004 sseerrii0201 banet523 o111na homegirl cho77j chuing77 ksb0219 tess00 bom124 sonming52 banet523 giyoming axcvdbs23 apffhdn1219 3ww1ww3 bongbong486 duk97031 deer9805 romantic09 dkdlfjqm758 muse62 chuchu22 siyun0813 nemu00 Vvvv1212 xxaaop syxx12 day59day obzee7 dudvv7 ahri0801 soso621 missedyou imanatural Sharon9sea seozzidooboo saone451 acac88 hyuna0721 2dayday pupu28";
 
 # 使用grep命令获取group1和group2中的重复元素，并组成一个新的数组
-userIds=($(echo "$watchids ${onlineIds[@]}" | tr ' ' '\n' | sort | uniq -d))
+userIds=($(echo "$watchIds ${onlineIds[@]}" | tr ' ' '\n' | sort | uniq -d))
 
 # 输出重复的元素
 echo "监控中在线主播：${userIds[@]}"
