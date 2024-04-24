14ea0e0f5c4db12ca4f0baef0567b50aba311f80
nbtu <62731238+nbtu@users.noreply.github.com>
2024-04-24 13:37:24 +0800

    Update pd.sh

--- pd.sh
+++ pd.sh
@@ -106,52 +106,52 @@ unreachableIds=()
 #for ((i=1; i<=1000; i++)); do
 #    echo "Round $i:"
     for userId in ${userIds[@]}; do
-        json=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" -H 'x-device-info:{"t":"webMobile","v":"1.0","ui":24631221}' -H "cookie:${cookie}" -X POST  "https://api.pandalive.co.kr/v1/live/play?action=watch&userId=${userId}"` 
-        hls=`echo $json| jq -r .PlayList.hls[0].url`
-        img=`echo $json| jq -r .media.thumbUrl`
-        startTime=`echo "$json"| jq -r .media.startTime`
-        echo "开始获取直播源"
-        if [ -n "$hls"  ] &&  [ "$hls" != null ]; then
-            echo "${userId}获取成功。"
-            echo "直播源：${hls}"
-            echo "开始创建房间" 
-            room=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${userToken}" --data-raw "{\"roomName\":\"Panda_${userId}\",\"password\":\"\",\"setting\": {\"hidden\": false}}" -X POST "${synctv}/api/room/create"`
-            roomid=`echo $room|jq -r .data.roomId`
-            roomToken=`echo $room|jq -r .data.token`
-            echo "房间id：$roomid"
-            if [ -n "$roomid"  ] && [ "$roomid" != null ]; then
-                jsondata="{\"name\": \"${userId}\",\"url\": \"${hls}\",\"type\": \"m3u8\",\"live\": true}"
-                #echo -e "$userId $roomid $roomToken $hls">> data.txt
-                #echo -e "添加$userId $hls">> $logfile
-                curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} -X POST "${synctv}/api/movie/clear"
-                curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} --data-raw "${jsondata}" -X POST "${synctv}/api/movie/push"
-                echo "$userId 已推送到Sync TV, removing from list"
-                #text="*J哥提醒你！！！！*\n\nPanda主播${userId}直播源已添加到SyncTV\n\n本场开播时间：$startTime（韩国时间快1小时）\n\n[直达地址，sync维护！](${synctv}/web/cinema/${roomid})\n\n[直达地址②，再次康康！](${m3u8site}?url=${userId})\n\n"
-                #text=$(echo "${text}" | sed 's/-/\\\\-/g')
-                #curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@Sexbjlive_Chat\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
-            elif [ "$roomid" = null ]; then
-                echo $room|jq -r .error
-                echo "创建房间失败，可能已有同名房间，跳过此ID" 
-            else 
-                echo "创建房间失败，跳过此ID（错误提示${room}）" 
-            fi
+        if grep -q "${userId}" data.txt; then
+            echo "The UID $uid exists in data.txt"
+        else
+            json=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" -H 'x-device-info:{"t":"webMobile","v":"1.0","ui":24631221}' -H "cookie:${cookie}" -X POST  "https://api.pandalive.co.kr/v1/live/play?action=watch&userId=${userId}"` 
+            hls=`echo $json| jq -r .PlayList.hls[0].url`
+            img=`echo $json| jq -r .media.thumbUrl`
+            startTime=`echo "$json"| jq -r .media.startTime`
+            echo "开始获取直播源"
+            if [ -n "$hls"  ] &&  [ "$hls" != null ]; then
+                echo "${userId}获取成功。"
+                echo "直播源：${hls}"
+                echo "开始创建房间" 
+                room=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${userToken}" --data-raw "{\"roomName\":\"Panda_${userId}\",\"password\":\"\",\"setting\": {\"hidden\": false}}" -X POST "${synctv}/api/room/create"`
+                roomid=`echo $room|jq -r .data.roomId`
+                roomToken=`echo $room|jq -r .data.token`
+                echo "房间id：$roomid"
+                if [ -n "$roomid"  ] && [ "$roomid" != null ]; then
+                    jsondata="{\"name\": \"${userId}\",\"url\": \"${hls}\",\"type\": \"m3u8\",\"live\": true}"
+                    #echo -e "$userId $roomid $roomToken $hls">> data.txt
+                    #echo -e "添加$userId $hls">> $logfile
+                    curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} -X POST "${synctv}/api/movie/clear"
+                    curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}" -w %{http_code} --data-raw "${jsondata}" -X POST "${synctv}/api/movie/push"
+                    echo "$userId 已推送到Sync TV, removing from list"
+                    #text="*J哥提醒你！！！！*\n\nPanda主播${userId}直播源已添加到SyncTV\n\n本场开播时间：$startTime（韩国时间快1小时）\n\n[直达地址，sync维护！](${synctv}/web/cinema/${roomid})\n\n[直达地址②，再次康康！](${m3u8site}?url=${userId})\n\n"
+                    #text=$(echo "${text}" | sed 's/-/\\\\-/g')
+                    #curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@Sexbjlive_Chat\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
+                elif [ "$roomid" = null ]; then
+                    echo $room|jq -r .error
+                    echo "创建房间失败，可能已有同名房间，跳过此ID" 
+                else 
+                    echo "创建房间失败，跳过此ID（错误提示${room}）" 
+                fi
             
-            if grep -q "${userId}" data.txt; then
-                echo "The UID $uid exists in data.txt"
-            else
-                echo "$userId 已推送到TG"
+                echo "$userId 推送到TG"
                 #text="*J哥提醒你！！！！*\n\nPanda主播${userId}直播源已添加到SyncTV\n\n本场开播时间：$startTime（韩国时间快1小时）\n\n[直达地址，让我康康！](${synctv}/web/cinema/${roomid})\n\n[直达地址②，再次康康！](${m3u8site}?url=${userId})\n\n"
                 text="*J哥提醒你！！！！*\n\nPanda主播${userId}直播源已添加到SyncTV\n\n本场开播时间：$startTime（韩国时间快1小时）\n\n[直达地址，sync维护中！](${m3u8site}?url=${userId})\n\n[直达地址②，再次康康！](${m3u8site}?url=${userId})\n\n"
                 text=$(echo "${text}" | sed 's/-/\\\\-/g')
                 curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@Sexbjlive_Chat\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
                 echo -e "$userId $roomid $roomToken $hls">> data.txt
                 echo -e "添加$userId $hls">> $logfile
+                reachableIds+=("$userId")
+            else 
+                echo "$userId 获取直播源失败！"
+                echo "错误提示：$json "
             fi
-            reachableIds+=("$userId")
-        else 
-            echo "$userId 获取直播源失败！"
-            echo "错误提示：$json "
-        fi   
+        fi
         echo "-----------`date`--------------"
         sleep 3
     done   
