be2c009d0d96fc7aab3b7074120a84e7f97b6231
nbtu <62731238+nbtu@users.noreply.github.com>
2024-05-11 07:58:28 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -18,7 +18,7 @@ do
         #else
             echo "$userId - $url 直播源失效, 删除房间, 删除记录" 
             echo -e "删除$userId $hls">> $logfile
-            curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -w "%{http_code}" -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}"  --data-raw "{\"roomid\": \"${roomid}\"}" -X POST "${synctv}/api/room/delete"
+            curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -w "%{http_code}" -H 'accept:application/json, text/plain, */*' -H "authorization:${roomToken}"  --data-raw "{\"roomid\": \"${roomid}\"}" -X POST "${synctv}/api/room/admin/delete"
             sed -i "\~$url~d" data.txt
             # 使用sed命令从文件中删除包含该URL的整行
         #fi
